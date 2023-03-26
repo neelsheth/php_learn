88b47c54f16f3b3afbd6abc208bdb1a31d13bc50
Neel Sheth <108276436+neelsheth@users.noreply.github.com>
2023-03-26 23:31:02 +0530

    Create ARRAY_OBJECT.php

---
+++ ARRAY_OBJECT.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Check extends CI_Controller
+{
+
+
+    public function index()
+    {   
+        //type 1
+        $this->db->where("variable",'web_settings');
+        $q = $this->db->get('settings');
+        echo "<pre>";
+        print_r($q->result_array());
+        $q2 = $q->result_array();
+        echo "<pre>";
+        print_r($q2[0]['value']);
+        $q3 = $q2[0]['value'];
+        //$myObject =json_decode($q3);
+        echo "<pre>";
+        print_r(var_dump($q3));
+
+
+
+        //type 2
+        // $query = $this->db->get_where('settings', array(
+        //     'variable' => 'web_settings'
+        // ));
+        // $count = $query->result();
+        // echo "<pre>";
+        // print_r($count);
+        // print_r($count[0]);
+        //  print_r($count[0]->value);
+
+
+      
+    }
+
+   
+
+}
